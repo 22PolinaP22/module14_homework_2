cbfcf7a6d508fe03b5d1c323209257fc309660b3
22PolinaP22 <159052346+22PolinaP22@users.noreply.github.com>
2024-03-18 23:36:03 +0700

    Update script.js

--- 14.task_5/script.js
+++ 14.task_5/script.js
@@ -51,10 +51,10 @@ function loadPhotos(apiData) {
     apiData.forEach(item => {
         const cardBlock =     `<div>
                                 <img
-                                  src="${item.download_url}"
+                                  src="${'https://via.placeholder.com/150'}" 
                                   style="width: 150px; margin-right: 30px"
                                 />
-                                <p>${item.author}</p>
+                                <p>${'https://via.placeholder.com/600'}</p>
                               </div>`;
         cards += cardBlock;
     });
